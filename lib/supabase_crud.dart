a9ccaa2d6c45c11120b108f950cbbfbee8f2bbbd
AugustKim <August@hmm-dev-macbook.local>
2024-02-09 20:51:10 +0900

    crud 텍스트폼과 버튼 구성 먼저

---
+++ lib/supabase_crud.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseCRUD extends StatelessWidget {
+  SupabaseCRUD({super.key});
+
+  final supabse = Supabase.instance.client;
+
+  final TextEditingController textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('CRUD')),
+      body: Container(
+        child: StreamBuilder(
+          stream: supabse.from('todo').stream(primaryKey: ['id']),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasError) {
+              return Text('Error 001');
+            }
+
+            final todos = snapshot.data!;
+
+            return ListView.builder(
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(
+                    Icons.check_circle,
+                    color: todos[index]['check'] ?? true
+                        ? Colors.green
+                        : Colors.grey,
+                  ),
+                  title: Text(todos[index]['todo']),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Flexible(
+                          child: IconButton(
+                              onPressed: () {}, icon: Icon(Icons.edit))),
+                      Flexible(
+                          child: IconButton(
+                              onPressed: () {}, icon: Icon(Icons.delete))),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('input'),
+                content: TextField(
+                  controller: textController,
+                  decoration: InputDecoration(
+                    hintText: 'input todo',
+                  ),
+                ),
+                actions: [
+                  ElevatedButton(onPressed: () {}, child: Text('CANCEL')),
+                  ElevatedButton(onPressed: () {}, child: Text('ADD')),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
